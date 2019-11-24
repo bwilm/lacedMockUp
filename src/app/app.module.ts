106d4c43a6b72ff741a1fd5ae556399953f2f9f5
bwilm <32461222+bwilm@users.noreply.github.com>
2019-11-23 23:47:22 -0500

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ProductGridComponent } from './components/product-grid/product-grid.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BestSellerComponent } from './components/best-seller/best-seller.component';
+import { FeatureSliderComponent } from './components/feature-slider/feature-slider.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    ProductGridComponent,
+    FooterComponent,
+    BestSellerComponent,
+    FeatureSliderComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
